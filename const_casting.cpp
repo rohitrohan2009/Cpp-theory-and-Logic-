e09e9103becb5a26b1970c03b438905c96d2c3fe
Bundang <psychic.killer8@gmail.com>
2019-10-26 03:37:38 +0530

    Update const_casting.cpp

--- const_casting.cpp
+++ const_casting.cpp
@@ -5,8 +5,8 @@ int main()
     int x = 12;
     const int y=7;//making y constant makes y a read-only variable
     x=y;//assigning the value of y to x thereby making x=7;
-    //if we typed y=x, a.k.a, assigning the value of x to y? it would give a read-only error as we
-    //can NOT assign any value whatsoever to a read-only variable y. It can NEVER change.
+        //if we typed y=x, a.k.a, assigning the value of x to y? it would give a read-only error as we
+        //can NOT assign any value whatsoever to a read-only variable y. It can NEVER change.
     cout<<x<<" "<< y;
 }
 
