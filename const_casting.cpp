34476e17eee3f5e1f0306cf2b6fada06aeb8f61c
Bundang <psychic.killer8@gmail.com>
2019-10-26 03:37:17 +0530

    Update and rename main.cpp to const_casting.cpp

---
+++ const_casting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 12;
+    const int y=7;//making y constant makes y a read-only variable
+    x=y;//assigning the value of y to x thereby making x=7;
+    //if we typed y=x, a.k.a, assigning the value of x to y? it would give a read-only error as we
+    //can NOT assign any value whatsoever to a read-only variable y. It can NEVER change.
+    cout<<x<<" "<< y;
+}
+
+//Output: '7 7'
+
